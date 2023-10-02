dae7b4af3d8b1bee2fb0ce756cce5824b5cf66ff
deliciaxox <deliciaxox@gmail.com>
2023-10-02 19:58:38 +0100

    portfolio added

---
+++ src/utils/Content.ts
@@ -0,0 +1,15 @@
+export const site_name = "Delicia - Frontend Developer";
+export const title = "Delicia - Frontend Developer";
+export const description =
+  "Delicia is a skilled Delaware-based Frontend Developer specializing in React, NextJS, Redux, Typescript and JavaScript. With a strong eye for design and attention to detail, she creates visually stunning and intuitive user interfaces. Committed to excellence, Delicia delivers exceptional web experiences that surpass expectations.";
+export const author = "Delicia Downer";
+export const locale_region = "en-us";
+export const locale = "en";
+export const imageLocation = "/assets/profile/delicia.png";
+
+// social media handles
+
+export const github = "https://github.com/deliciaxox";
+export const twitter = "https://github.com/deliciaxox";
+export const linkedin = "https://github.com/deliciaxox";
+export const resume = "";
